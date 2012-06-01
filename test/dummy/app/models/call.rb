3635e36fc321077d560348b93de7b2d4a42942a3
Daniel Berkompas <daniel@clearsightstudio.com>
2012-06-01 11:52:24 -0700

    Cleaning up the code with some metaprogramming

--- test/dummy/app/models/call.rb
+++ test/dummy/app/models/call.rb
@@ -5,6 +5,7 @@ class Call < ActiveRecord::Base
   menu :opening_menu do
     play "/test-sound.mp3"
     say "This is the first menu", voice: "woman", language: "en-gb"
+    sms "Whatever I want to say", to: 3602636483, from: 0000000000
     gather do
       prompt 1, "Press 1 to go to the second menu", menu: :second_menu 
       prompt 2, "Press 2 to go to the third menu", menu: :third_menu
